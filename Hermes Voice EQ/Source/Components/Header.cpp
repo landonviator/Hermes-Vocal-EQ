42f4772b393235b66b84172712158e80fa4cd22c
Landon <33662627+landonviator@users.noreply.github.com>
2023-05-07 16:06:36 -0500

    Add male female buttons

--- Hermes Voice EQ/Source/Components/Header.cpp
+++ Hermes Voice EQ/Source/Components/Header.cpp
@@ -9,6 +9,12 @@ Header::Header()
     
     _dropShadow = std::make_unique<juce::DropShadower>(juce::DropShadow(juce::Colours::black.withAlpha(0.5f), 5, {}));
     _dropShadow->setOwner(this);
+    
+    // buttons
+    for (auto& button : _buttons)
+    {
+        initButtons(*button);
+    }
 }
 
 Header::~Header()
@@ -17,25 +23,8 @@ Header::~Header()
 
 void Header::paint (juce::Graphics& g)
 {
-    // init color info from parent
-    auto* parent = dynamic_cast<HermesVoiceEQAudioProcessorEditor*>(getParentComponent());
-    if (parent != nullptr)
-    {
-        auto currentBGColor = parent->getThemeData().getAuxBackgroundColor();
-        auto nonContrastColor = parent->getThemeData().getWidgetFillColor();
-        auto contrastColor = parent->getThemeData().getMainBackgroundColor();
-        
-        _settingsButton.setColour(juce::TextButton::ColourIds::textColourOnId, nonContrastColor);
-        _settingsButton.setColour(juce::TextButton::ColourIds::textColourOffId, nonContrastColor);
-        
-        auto black = juce::Colour::fromRGB(0, 0, 0);
-        bool isHighContrast = currentBGColor == black;
-        
-        g.setColour(black.withAlpha(isHighContrast ? 1.0f : 0.2f));
-        _settingsButton.setColour(juce::TextButton::ColourIds::textColourOnId, isHighContrast ? contrastColor : nonContrastColor);
-        _settingsButton.setColour(juce::TextButton::ColourIds::textColourOffId, isHighContrast ? contrastColor : nonContrastColor);
-    }
-
+    // update colors on theme change
+    updateButtonColors(g);
     
     // set background color
     g.fillRect(getLocalBounds());
@@ -58,10 +47,17 @@ void Header::paint (juce::Graphics& g)
 
 void Header::resized()
 {
-    auto buttonWidth = getHeight() * 0.75;
+    // settings
+    auto squareButtonWidth = getHeight() * 0.75;
     auto padding = getHeight() * 0.125;
+    _settingsButton.setBounds(getWidth() - squareButtonWidth - padding, padding, squareButtonWidth, squareButtonWidth);
     
-    _settingsButton.setBounds(getWidth() - buttonWidth - padding, padding, buttonWidth, buttonWidth);
+    // buttons
+    auto buttonHeight = getHeight() * 0.75;
+    auto buttonWidth = buttonHeight * 2.0;
+    auto buttonTopPadding = getHeight() * 0.125;
+    _maleButton.setBounds(getWidth() * 0.5 - buttonWidth, buttonTopPadding, buttonWidth, buttonHeight);
+    _femaleButton.setBounds(_maleButton.getRight(), _maleButton.getY(), buttonWidth, buttonHeight);
 }
 
 #pragma mark Buttons
@@ -78,4 +74,49 @@ void Header::setSettingsButtonProps()
 bool Header::isSettingsActive()
 {
     return _settingsButton.getToggleState();
+}
+
+void Header::initButtons(viator_gui::TextButton &button)
+{
+    _maleButton.setButtonText("Male");
+    _maleButton.setRadioGroupId(1);
+    _femaleButton.setButtonText("Female");
+    _femaleButton.setRadioGroupId(1);
+    button.setColour(juce::TextButton::ColourIds::buttonColourId, juce::Colour::fromRGB(74, 81, 98).darker(0.5).withAlpha(0.3f));
+    button.setColour(juce::TextButton::ColourIds::buttonOnColourId, juce::Colour::fromRGB(74, 81, 98));
+    button.setColour(juce::ComboBox::outlineColourId, juce::Colours::transparentBlack);
+    button.setColour(juce::TextButton::ColourIds::textColourOnId, _textColor);
+    button.setColour(juce::TextButton::ColourIds::textColourOffId, _textColor);
+    button.setLookAndFeel(&_customButton);
+    addAndMakeVisible(button);
+}
+
+void Header::updateButtonColors(juce::Graphics& g)
+{
+    // init color info from parent
+    auto* parent = dynamic_cast<HermesVoiceEQAudioProcessorEditor*>(getParentComponent());
+    if (parent != nullptr)
+    {
+        auto currentBGColor = parent->getThemeData().getAuxBackgroundColor();
+        auto nonContrastColor = parent->getThemeData().getWidgetFillColor();
+        auto contrastColor = parent->getThemeData().getMainBackgroundColor();
+        
+        _settingsButton.setColour(juce::TextButton::ColourIds::textColourOnId, nonContrastColor);
+        _settingsButton.setColour(juce::TextButton::ColourIds::textColourOffId, nonContrastColor);
+        _maleButton.setColour(juce::TextButton::ColourIds::textColourOnId, nonContrastColor);
+        _maleButton.setColour(juce::TextButton::ColourIds::textColourOffId, nonContrastColor);
+        _femaleButton.setColour(juce::TextButton::ColourIds::textColourOnId, nonContrastColor);
+        _femaleButton.setColour(juce::TextButton::ColourIds::textColourOffId, nonContrastColor);
+        
+        auto black = juce::Colour::fromRGB(0, 0, 0);
+        bool isHighContrast = currentBGColor == black;
+        
+        g.setColour(black.withAlpha(isHighContrast ? 1.0f : 0.2f));
+        _settingsButton.setColour(juce::TextButton::ColourIds::textColourOnId, isHighContrast ? contrastColor : nonContrastColor);
+        _settingsButton.setColour(juce::TextButton::ColourIds::textColourOffId, isHighContrast ? contrastColor : nonContrastColor);
+        _maleButton.setColour(juce::TextButton::ColourIds::textColourOnId, isHighContrast ? contrastColor : nonContrastColor);
+        _maleButton.setColour(juce::TextButton::ColourIds::textColourOffId, isHighContrast ? contrastColor : nonContrastColor);
+        _femaleButton.setColour(juce::TextButton::ColourIds::textColourOnId, isHighContrast ? contrastColor : nonContrastColor);
+        _femaleButton.setColour(juce::TextButton::ColourIds::textColourOffId, isHighContrast ? contrastColor : nonContrastColor);
+    }
 }
--- Hermes Voice EQ/Source/Components/Header.h
+++ Hermes Voice EQ/Source/Components/Header.h
@@ -24,13 +24,26 @@ private:
     // shadow
     std::unique_ptr<juce::DropShadower> _dropShadow;
     
+    // buttons
+    viator_gui::TextButton _maleButton;
+    viator_gui::TextButton _femaleButton;
+    viator_gui::CustomTextButton _customButton;
+    
 // methods
 private:
     void setSettingsButtonProps();
+    void initButtons(viator_gui::TextButton& button);
+    void updateButtonColors(juce::Graphics& g);
     
 // vars
 private:
+    // colors
+    const juce::Colour _innerBgColor = juce::Colour::fromRGB(40, 44, 52);
     const juce::Colour _textColor = juce::Colour::fromRGB(161, 168, 181).darker(0.3f);
+    std::vector<viator_gui::TextButton*> _buttons =
+    {
+        &_maleButton, &_femaleButton
+    };
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Header)
 };
